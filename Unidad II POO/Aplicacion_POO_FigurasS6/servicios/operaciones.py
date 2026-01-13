5b133ed60dd3f818a963f98a402a97b3ee060bbb
Leiber <le.correab@uea.edu.ec>
2026-01-13 16:35:37 -0500

    Agrega funciones de operaciones con polimorfismo

--- Unidad II POO/Aplicacion_POO_FigurasS6/servicios/operaciones.py
+++ Unidad II POO/Aplicacion_POO_FigurasS6/servicios/operaciones.py
@@ -1,1 +1,10 @@
+# servicios/operaciones.py
+
+def mostrar_area(figura):
+    """
+    Función que recibe un objeto de tipo Figura.
+    Aplica polimorfismo, ya que el método calcular_area()
+    se ejecuta según la clase real del objeto recibido.
+    """
+    print(f"Área calculada: {figura.calcular_area():.2f}")
 
